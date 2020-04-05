c18c3e6677819cdc57a91034501eac72a686807f
Gustavo Santos <gustavo.1.santos-ext@jci.com>
2020-04-05 17:51:40 +0100

    open package to javafx

--- course.java9.jokeui/src/module-info.java
+++ course.java9.jokeui/src/module-info.java
@@ -1,8 +1,10 @@
-open module course.java9.jokeui {
+module course.java9.jokeui {
     requires course.java9.jokeserver;
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
 
     uses course.java9.jokeserver.JokeServer;
+
+    opens course.java9.jokeui to javafx.fxml, javafx.graphics; // only open the package and define to who this is open
 }
